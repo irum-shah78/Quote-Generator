f9917ea82258e1a85f7533b454cdeb34daa17fd9
irum-shah78 <irams4812@gmail.com>
2024-03-22 22:25:17 +0500

    Completed

--- script.js
+++ script.js
@@ -13,5 +13,5 @@ async function getQuote(url) {
 getQuote(apiURL);
 
 function Tweet(){
-  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML, "Tweet Window", "width=600, height=300");
+  window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "---- by " + author.innerHTML, "Tweet Window", "width=600, height=300");
 }
